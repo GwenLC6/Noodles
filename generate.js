8956dd8c2da57ebb481282c9c6e6363db4815577
Iva <iva_strat@hotmail.com>
2020-12-02 19:08:17 +0000

    generator all done:)

--- generate.js
+++ generate.js
@@ -20,9 +20,62 @@ function hasIngredient(checkbox) {
 }
 
 function findRecipes() {
-    var recipes = [1, 2, 3];
-    if (ingredients[0]) {
-        recipes[0] = "soy sauce";
-        alert("Please see recipes n. " + recipes + "!")
+    var recipes = [];
+    if ((ingredients[7] && noodles[0]) || (ingredients[12] && noodles[0])) {
+        var this_recipe = {};
+        this_recipe['value'] = "One-wok Vegan Stir-fry";
+        this_recipe['href'] = "vegan.html";
+        recipes.push(this_recipe);
     }
+    if (ingredients[3] || noodles[0]) {
+        var this_recipe = {};
+        this_recipe['value'] = "Hearty Soup Noodle";
+        this_recipe['href'] = "soup.html";
+        recipes.push(this_recipe);
+    }
+    if (noodles[1]) {
+        var this_recipe = {};
+        this_recipe['value'] = "Cold Soba Treat";
+        this_recipe['href'] = "soba.html";
+        recipes.push(this_recipe);
+    }
+    if (noodles[2]) {
+        var this_recipe = {};
+        this_recipe['value'] = "Spicy Pasta Fusion";
+        this_recipe['href'] = "pasta.html";
+        recipes.push(this_recipe);
+    }
+    if (ingredients[4]) {
+        var this_recipe = {};
+        this_recipe['value'] = "Sweet n Sour Peanut Butter Noodles";
+        this_recipe['href'] = "peanut.html";
+        recipes.push(this_recipe);
+    }
+
+    var modalDiv = document.getElementById("recipe-results2");
+    recipes.forEach(element => {
+        var div = document.createElement('div');
+        var linkToRecipe = document.createElement('a');
+        div.setAttribute('class', "big-text");
+        linkToRecipe.setAttribute('class', "light");
+        linkToRecipe.innerText = element.value;
+        linkToRecipe.setAttribute('href', element.href);
+        modalDiv.appendChild(div);
+        div.appendChild(linkToRecipe);
+    });
+
+    showRecipes();
+}
+
+function showRecipes() {
+    var modal = document.getElementById("recipe-results");
+    modal.style.display = "block";
+}
+
+var closeButton = document.getElementById("closeResults");
+function closeResults() {
+    var modal = document.getElementById("recipe-results");
+    modal.style.display = "none";
+    var modalDiv = document.getElementById("recipe-results2");
+    modalDiv.innerHTML = "";
 }
